de33d3c8841e3673a5f999c77824ebcc07b0d8f0
AlwaysYou <agurtohans@gmail.com>
2018-03-04 13:05:11 -0500

    modulo:productos admin/models

--- src/apps/productos/admin.py
+++ src/apps/productos/admin.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Curso
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+	pass
--- src/apps/productos/models.py
+++ src/apps/productos/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from uuslug import uuslug
+from filebrowser.fields import FileBrowseField
 
 # Create your models here.
 
@@ -9,9 +10,6 @@ class Categoria(models.Model):
     nombre = models.CharField('Nombre', max_length=120)
     slug = models.SlugField('slug', max_length=180, blank=True)
     position = models.SmallIntegerField('Posición', default=0)
-
-    class Meta:
-        abstract = True
 
     def save(self, *args, **kwargs):
         try:
@@ -25,3 +23,27 @@ class Categoria(models.Model):
 
     def __unicode__(self):
         return self.nombre
+
+
+class Curso(models.Model):
+    fk_categoria = models.ForeignKey(Categoria,
+    							     related_name="prod_cat",
+    							     verbose_name="Categoria",
+    							     blank=True)
+
+    nombre = models.CharField('Nombre del Curso', max_length=120)
+    position = models.SmallIntegerField(u'Posición', default=0)
+    codigo = models.CharField(u"Código", max_length=400)
+    precio = models.DecimalField('Precio Referencia', max_digits=12, decimal_places=2, default=0)
+    img_curso = FileBrowseField('Imagen del Curso',
+                                max_length=200, blank=True,
+                                extensions=['.jpg', '.png', '.gif'],
+                                directory='imagen_curso')
+
+    class Meta:
+        verbose_name = u'Curso'
+        verbose_name_plural = u'Curso'
+
+    def __str__(self):
+        return u'%s' % self.nombre
+
