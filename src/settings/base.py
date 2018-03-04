c66c5b8c59684a1d2d894f97b2ece520b414edec
AlwaysYou <agurtohans@gmail.com>
2018-03-04 11:01:04 -0500

    integracion: primera plantilla(home)

--- src/settings/base.py
+++ src/settings/base.py
@@ -137,7 +137,7 @@ STATIC_ROOT = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+print(STATICFILES_DIRS, "<- STATICFILES_DIRS")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
