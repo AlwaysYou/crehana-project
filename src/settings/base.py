b7e074fa6a88583a94aecc874e6a71bd485f3690
hans takeshi <agurtohans@gmail.com>
2018-04-08 17:31:23 -0500

    added django-toolbar

--- src/settings/base.py
+++ src/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'filebrowser',
     'apps.web',
     'apps.pedidos',
@@ -49,18 +50,19 @@ INSTALLED_APPS = [
     'admin_reorder',
 
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'admin_reorder.middleware.ModelAdminReorder',
 
 ]
+INTERNAL_IPS = ('127.0.0.1',)
 
 ROOT_URLCONF = 'urls'
 
--- src/urls.py
+++ src/urls.py
@@ -9,15 +9,18 @@ from apps.usuarios import urls as usuarios_urls
 from apps.productos import urls as productos_urls
 from apps.web import urls as web_urls
 from apps.pedidos import urls as pedidos_urls
+import debug_toolbar
+
 
 urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] +[
     url(r'^admin/', admin.site.urls),
     url(r'^admin/filebrowser/', include(site.urls)),
     url(r'', include(web_urls, namespace='web')),
     url(r'^usuarios/', include(usuarios_urls, namespace='usuarios')),
     url(r'^productos/', include(productos_urls, namespace='productos')),
     url(r'^pedidos/', include(pedidos_urls, namespace='pedidos')),
-
 
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
      static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
