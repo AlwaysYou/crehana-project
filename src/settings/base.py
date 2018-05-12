b1a4c0f0f356a8ab5d603178b817fd54d545b540
hans takeshi <agurtohans@gmail.com>
2018-05-11 23:22:10 -0500

    base fixed

--- src/settings/base.py
+++ src/settings/base.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 # STATIC and MEDIA
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
