f032265b7d3278e5e7abba626ffb23e81ff0cb4d
hans takeshi <agurtohans@gmail.com>
2018-05-11 23:24:02 -0500

    base media trying to watch images

--- src/settings/base.py
+++ src/settings/base.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = ''
 
 # EMAIL
 EMAIL_HOST = ENV.get('EMAIL_HOST', '')
